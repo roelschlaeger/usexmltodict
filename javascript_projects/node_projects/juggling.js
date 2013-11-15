171e94ff775010025f86909127b25fe56998f61b
roelschlaeger <roelschlaeger@gmail.com>
2013-11-15 15:44:40 -0600

    juggling.js successful

--- javascript_projects/node_projects/juggling.js
+++ javascript_projects/node_projects/juggling.js
@@ -2,46 +2,52 @@
 // vim:ts=4:sw=4:tw=0:wm=0:et:nowrap
 
 // Created:       Fri 15 Nov 2013 11:42:36 AM CST
-// Last Modified: Fri 15 Nov 2013 01:28:34 PM CST
+// Last Modified: Fri 15 Nov 2013 03:42:59 PM CST
 
 http = require('http');
 
-var done = 0;
+var TIMEOUT = 1000;
 
-function one(arg, result) {
+var done = 0;
+var result = { 
+    's1' : '', 
+    's2' : '', 
+    's3' : ''
+};
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+var arg3 = process.argv[4];
+
+function callback(res, result_index) {
     var str = '';
-    console.log("get: ", arg);
-    http.get(arg, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {str += chunk});
-        res.on('end', function() {result = str; done += 1});
-        res.on('error', console.error);
-    });
+//  console.log("starting");
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {str += chunk});
+    res.on('end', function() {result[result_index] = str; done += 1});
+    res.on('error', console.error);
+};
+        
+function one(arg, result) {
+    http.get(arg, function(res) {callback(res, result)});
 };
 
-arg1 = process.argv[2];
-console.log("arg1=", arg1);
+// console.log(process.argv);
 
-arg2 = process.argv[3];
-console.log("arg2=", arg2);
+one(arg1, 's1');
+one(arg2, 's2');
+one(arg3, 's3');
 
-arg3 = process.argv[4];
-console.log("arg3=", arg3);
+function checker() {
+//  console.log("done=", done);
+    if (done === 3) {
+//      console.log("Hooray");
+        console.log(result['s1']);
+        console.log(result['s2']);
+        console.log(result['s3']);
+    } else {
+        setTimeout(checker, TIMEOUT);
+    }
+}
 
-var v1, v2, v3;
-one(arg1, v1);
-one(arg2, v2);
-one(arg3, v3);
-
-console.log(done);
-while (done < 1);
-console.log(done);
-while (done < 2);
-console.log(done);
-while (done < 3);
-console.log(done);
-
-console.log(v1);
-console.log(v2);
-console.log(v3);
+setTimeout(checker, TIMEOUT);
 
