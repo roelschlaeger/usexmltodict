e544b7a46248ed1662e8bee1a00716fe9468ab5e
roelschlaeger <roelschlaeger@gmail.com>
2014-01-17 12:55:24 -0600

    Add publish.log to move files to Dropbox/Geocaching

---
+++ python_projects/rooter/publish.bat
@@ -0,0 +1,3 @@
+@rem Move rooter.py and latest_log.py to \Dropbox\Geocaching
+@copy rooter.py c:\users\"Robert Oelschlaeger"\Dropbox\Geocaching
+@copy latest_log.py c:\users\"Robert Oelschlaeger"\Dropbox\Geocaching
