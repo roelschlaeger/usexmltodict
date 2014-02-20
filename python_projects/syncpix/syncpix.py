dade09421b48b125bc0ebd9e8d2540a6b672e52d
roelschlaeger <roelschlaeger@gmail.com>
2014-02-20 11:32:56 -0600

    Update for 20140218

--- python_projects/syncpix/syncpix.py
+++ python_projects/syncpix/syncpix.py
@@ -2,7 +2,7 @@
 # vim:ts=4:sw=4:tw=0:wm=0:et:foldlevel=99:fileencoding=utf-8
 
 # Created:       Fri 03 Jan 2014 03:26:18 PM CST
-# Last Modified: Sun 09 Feb 2014 12:16:35 PM CST
+# Last Modified: Thu 20 Feb 2014 11:31:58 AM CST
 
 """
 SYNOPSIS
@@ -287,12 +287,12 @@ if __name__ == '__main__':
     import traceback
 #   import os
 
-    DATE = "20140208"
+    DATE = "20140218"
     HOME = r"C:\Users\Robert Oelschlaeger"
     PIXDIR = r"%s\Google Drive\Caching Pictures\%s" % (HOME, DATE)
     GPXFILE = r"%s\explorist_results_%s.gpx" % (PIXDIR, DATE)
 
-    ROUTE_NAME = "topo732b - Chaffee MO"
+    ROUTE_NAME = "topo735 - Saint Louis MO"
     TIMEZONE = "CST"
 
     ########################################################################
