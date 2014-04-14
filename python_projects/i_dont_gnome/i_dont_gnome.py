a70f5a8441781a568190d0f5139d7218f23d6f8b
roelschlaeger <roelschlaeger@gmail.com>
2014-04-14 15:19:44 -0500

    Check in

---
+++ python_projects/i_dont_gnome/i_dont_gnome.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# vim:ts=4:sw=4:tw=0:wm=0:et:foldlevel=99:fileencoding=utf-8:ft=python
+
+# Created:       Mon 14 Apr 2014 03:08:13 PM CDT
+# Last Modified: Mon 14 Apr 2014 03:10:45 PM CDT
+
+"""
+SYNOPSIS
+
+    TODO helloworld [-h] [-v,--verbose] [--version]
+
+DESCRIPTION
+
+    TODO This describes how to use this script.
+    This docstring will be printed by the script if there is an error or
+    if the user requests help (-h or --help).
+
+EXAMPLES
+
+    TODO: Show some examples of how to use this script.
+
+EXIT STATUS
+
+    TODO: List exit codes
+
+AUTHOR
+
+    TODO: Robert Oelschlaeger <roelsch2009@gmail.com>
+
+LICENSE
+
+    This script is in the public domain.
+
+VERSION
+
+"""
+
+__VERSION__ = "0.0.1"
+
+########################################################################
+
+"""The actual coordinates are: N 39 50.6B0 W 88 57.DE5 B is either 5,6,7,8 D is
+either 5,6,7,8 E is either 0,1,2,3 Good Luck"""
+
+print "index\tlatitude\tlongitude"
+index = 0
+for B in [5, 6, 7, 8]:
+    for D in [5, 6, 7, 8]:
+        for E in [0, 1, 2, 3]:
+            lat = "N39 50.6%d0" % B
+            lon = "W88 57.%d%d5" % (D, E)
+            index += 1
+            print "%d\t%s\t%s" % (index, lat, lon)
+
+if __name__ == '__main__':
+
+    import sys
+    import os
+    import traceback
+    import optparse
+    import time
+
+#from pexpect import run, spawn
+
+# Uncomment the following section if you want readline history support.
+#import readline, atexit
+#histfile = os.path.join(os.environ['HOME'], '.TODO_history')
+#try:
+#    readline.read_history_file(histfile)
+#except IOError:
+#    pass
+#atexit.register(readline.write_history_file, histfile)
+
+########################################################################
+
+    def main():
+
+        global options, args
+
+        # TODO: Do something more interesting here...
+        print 'Hello world!'
+
+########################################################################
+
+    try:
+        START_TIME = time.time()
+
+        PARSER = optparse.OptionParser(
+            formatter=optparse.TitledHelpFormatter(),
+            usage=globals()['__doc__'],
+            version=__VERSION__
+        )
+
+        PARSER.add_option(
+            '-v',
+            '--verbose',
+            action='store_true',
+            default=False,
+            help='verbose output'
+        )
+
+        (OPTIONS, ARGS) = PARSER.parse_args()
+
+        #   if len(ARGS) < 1:
+        #       PARSER.error ('missing argument')
+
+        if OPTIONS.verbose:
+            print time.asctime()
+
+        EXIT_CODE = main()
+
+        if EXIT_CODE is None:
+            EXIT_CODE = 0
+
+        if OPTIONS.verbose:
+            print time.asctime()
+            print 'TOTAL TIME IN MINUTES:',
+            print (time.time() - START_TIME) / 60.0
+
+        sys.exit(EXIT_CODE)
+
+    except KeyboardInterrupt, error_exception:        # Ctrl-C
+        raise error_exception
+
+    except SystemExit, error_exception:               # sys.exit()
+        raise error_exception
+
+    except Exception, error_exception:
+        print 'ERROR, UNEXPECTED EXCEPTION'
+        print str(error_exception)
+        traceback.print_exc()
+        os._exit(1)
+
+# end of file
