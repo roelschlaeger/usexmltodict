173aee3ae1bb9a1ad5d9e012922e088ce87a2aed
roelschlaeger <roelschlaeger@gmail.com>
2014-04-29 17:44:08 -0500

    Separate by state

--- python_projects/louis_challenge/louis.py
+++ python_projects/louis_challenge/louis.py
@@ -2,7 +2,7 @@
 # vim:ts=4:sw=4:tw=0:wm=0:et:foldlevel=99:fileencoding=utf-8:ft=python
 
 # Created:       Tue 29 Apr 2014 04:24:08 PM CDT
-# Last Modified: Tue 29 Apr 2014 04:56:51 PM CDT
+# Last Modified: Tue 29 Apr 2014 05:41:06 PM CDT
 
 """
 SYNOPSIS
@@ -13,7 +13,8 @@ DESCRIPTION
 
     This script computes values for certification of the
     "Nothing to L.O.U.I.S. Challenge"
-    http://www.geocaching.com/geocache/GC4H5VZ_nothing-to-l-o-u-i-s-challenge?guid=19f1a3ae-192a-46a0-9317-76ad28777859
+    http://www.geocaching.com/geocache/GC4H5VZ_nothing-to-l-o-u-i-s-challenge
+    ?guid=19f1a3ae-192a-46a0-9317-76ad28777859
 
     Input data is contained in the file "390613.gpx"
 
@@ -37,27 +38,31 @@ VERSION
 
 """
 
-__VERSION__ = "0.0.2"
+__VERSION__ = "0.0.3"
 
 ########################################################################
 
 from xml.etree import ElementTree as ET
-from collections import Counter
+from collections import Counter, defaultdict
 
 ########################################################################
 
 FILENAME = "390613.gpx"
+"""The filename being read; contains found cache information"""
 
 ########################################################################
 
 
-def get_first_gc(gc_name_list, three_letter_name_list):
+def get_first_gc(gc_namelist, three_letter_namelist):
+    """Locate the lexicographically first cache for each element in
+three_letter_namelist"""
 
-    from collections import defaultdict
     first_gc_dict = defaultdict(None)
 
-    for three_letter_name, gc_name in zip(three_letter_name_list, gc_name_list):
+    for three_letter_name, gc_name in zip(three_letter_namelist, gc_namelist):
         if first_gc_dict.get(three_letter_name) is None:
+            first_gc_dict[three_letter_name] = gc_name
+        elif len(first_gc_dict[three_letter_name]) > len(gc_name):
             first_gc_dict[three_letter_name] = gc_name
         elif first_gc_dict[three_letter_name] > gc_name:
             first_gc_dict[three_letter_name] = gc_name
@@ -68,40 +73,52 @@ def get_first_gc(gc_name_list, three_letter_name_list):
 
 
 def process(filename):
-    """Get counts of the number of GCx files that have been found"""
+    """Get counts of the number of GCx files that have been found on a
+per-state basis"""
 
     print __VERSION__
     print "Reading from %s" % filename
 
     tree = ET.parse(filename)
     root = tree.getroot()
-    wpts = root.findall(root.tag.replace("gpx","wpt"))
+    wpts = root.findall(root.tag.replace("gpx", "wpt"))
     print "%d geocaches processed" % len(wpts)
 
-    # get the GC names
-    gc = [w[1].text for w in wpts]
+    # get all of the GC names
+    raw_gc = [w[1].text for w in wpts]
 
-    # get the first three letters of each GC name
-    tlc = [x[:3] for x in gc]
+    # get all of the states
+    raw_states = [w[7][9].text for w in wpts]
+
+    # get the first three letters of each GC name for Illinois and Missouri
+    tlc = ["%s_%s" % (st, x[:3])
+           for x, st in zip(raw_gc, raw_states)
+           if st in ["Illinois", "Missouri"]
+           ]
+
+    # get GC names for Illinois and Missouri
+    gc = [x for x, st in zip(raw_gc, raw_states)
+          if st in ["Illinois", "Missouri"]
+          ]
 
     # count them up
     c = Counter(tlc)
 
-    # print the
+    # print the result
     result = sorted(c.keys())
     print "%d results" % len(result)
 
-    print "%3s\t%s" % ("key", "length")
+    # show the sorted counts for each key
+    print "%s\t%s" % ("key", "length")
     for k in sorted(c.keys()):
-        print "%3s\t%s" % (k, c[k])
+        print "%s\t%s" % (k, c[k])
     print
 
+    # show the first GC for each key
     result = get_first_gc(gc, tlc)
-#   from pprint import pprint
-#   pprint(result)
-    print "%3s\t%s" % ("key", "GC")
+    print "%s\t%s" % ("key", "GC")
     for k in sorted(result.keys()):
-        print "%3s\t%s" % (k, result[k])
+        print "%s\t%s" % (k, result[k])
 
 ########################################################################
 
