bbf5fae3b84cfe1cc49d45c3185353d28e43df91
roelschlaeger <roelschlaeger@gmail.com>
2014-12-20 11:51:14 -0600

    css tweaking; JSON-formatted wpt[8]

--- javascript_projects/node_projects/eggly/eggly.js
+++ javascript_projects/node_projects/eggly/eggly.js
@@ -1,8 +1,6 @@
 angular.module('eggly_app', []).
 controller('controller', ['$scope', function($scope)
 {
-    // var wpts = test.gpx.wpt;
-    // console.log(wpts.length);
     $scope.wpts = test.gpx.wpt;
-    // console.log($scope.wpts.length);
+    $scope.show_wpts = angular.toJson($scope.wpts[8],1);
 }]);
