e038122f94de8fdd9be504009e8326baa8252421
roelschlaeger <roelschlaeger@gmail.com>
2015-07-11 13:54:05 -0500

    Video 3

--- python_projects/flask-intro/app.py
+++ python_projects/flask-intro/app.py
@@ -1,12 +1,32 @@
 # vim:ts=4:sw=4:tw=0:wm=0:et
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, \
+    url_for, session, flash
+
+from functools import wraps
 
 app = Flask(__name__)
 
+# TODO -- Security Problem
+app.secret_key = "Shhh! Don't Tell!"
 
-@app.route('/')
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# use decorators to link the function to a url
+@app.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return 'Hello, world!'
+    return render_template('index.html')
 
 
 @app.route('/welcome')
@@ -17,8 +37,6 @@ def welcome():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-#   from pprint import pprint
-#   pprint(request.method)
     if request.method == 'POST':
         if (
             request.form['username'] != 'admin'
@@ -27,8 +45,18 @@ def login():
         ):
             error = 'Invalid credentials; please try again'
         else:
-            return redirect(url_for('home'))
+            session['logged_in'] = True
+            flash('You are logged in.')
+            return redirect(url_for('welcome'))
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You are logged out.')
+    return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
     app.run(debug=True)
