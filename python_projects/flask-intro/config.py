8e85477959314f673f703361182d9c300822f7b8
roelschlaeger <roelschlaeger@gmail.com>
2015-07-28 16:45:15 -0500

    Transition to Postgres

--- python_projects/flask-intro/config.py
+++ python_projects/flask-intro/config.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '&\xcf\xb7M\xe3e\x92W+\x83\x11\xd0s\x89!r~H]e\x99P\xb6='
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    print SQLALCHEMY_DATABASE_URI
 
 
 class DevelopmentConfig(BaseConfig):
--- python_projects/flask-intro/db_create.py
+++ python_projects/flask-intro/db_create.py
@@ -12,6 +12,7 @@ db.create_all()
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
 db.session.add(BlogPost("Hello", "Hello from the shell"))
+db.session.add(BlogPost("Well", "Working with Postgres"))
 
 # commit
 db.session.commit()
