ef0ae35c4f3e9aaf7531cd015a7ae9180e410751
roelschlaeger <roelschlaeger@gmail.com>
2016-02-16 15:20:47 -0600

    Checkpoint after Video 5

--- python_projects/tkinter_tutorial/step1.py
+++ python_projects/tkinter_tutorial/step1.py
@@ -3,20 +3,26 @@ if sys.version_info < (3,0):
     raise DeprecationWarning("Only supported for Python 3.x")
 
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ("Verdana", 12)
 
 # Initial coding from
+# Styling GUIs and windows in Python 3 - Tkinter tutorial Python 3.4
+#
 # https://www.youtube.com/watch?v=A0gaXfM1UN0 Video 2
 # https://www.youtube.com/watch?v=jBUpjijYtCk Video 4
+# https://www.youtube.com/watch?v=oV68QJJUXTU Video 5
 
 class SeaofBTCapp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.iconbitmap(self, default="clienticon.ico")
+        tk.Tk.wm_title(self, "Sea of BTC Client")
+
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand=True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
@@ -30,29 +36,29 @@ class SeaofBTCapp(tk.Tk):
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
-
         frame = self.frames[cont]
         frame.tkraise()
 
-def qf(stringtoprint):
-    print(stringtoprint)
+# def qf(stringtoprint):
+#     print(stringtoprint)
 
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(
+        button1 = ttk.Button(
             self,
             text="Visit Page 1",
             command=lambda: controller.show_frame(PageOne)
         )
         button1.pack()
 
-        button2 = tk.Button(
+        button2 = ttk.Button(
             self,
             text="Visit Page 2",
             command=lambda: controller.show_frame(PageTwo)
@@ -65,14 +71,14 @@ class PageOne(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Page One", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
-        button1 = tk.Button(
+        button1 = ttk.Button(
             self,
             text="Back to Home",
             command=lambda: controller.show_frame(StartPage)
         )
         button1.pack()
 
-        button2 = tk.Button(
+        button2 = ttk.Button(
             self,
             text="Visit Page 2",
             command=lambda: controller.show_frame(PageTwo)
@@ -85,14 +91,14 @@ class PageTwo(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Page Two", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
-        button1 = tk.Button(
+        button1 = ttk.Button(
             self,
             text="Back to Home",
             command=lambda: controller.show_frame(StartPage)
         )
         button1.pack()
 
-        button2 = tk.Button(
+        button2 = ttk.Button(
             self,
             text="Visit Page 1",
             command=lambda: controller.show_frame(PageOne)
