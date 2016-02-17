85e764cb87d16a1f812c1b9908d70cba656d18d0
roelschlaeger <roelschlaeger@gmail.com>
2016-02-17 09:30:40 -0600

    Checkpoint after Video 6; using Python 3.4

--- python_projects/tkinter_tutorial/go.bat
+++ python_projects/tkinter_tutorial/go.bat
@@ -1,1 +1,3 @@
-python35 step1.py
+@rem python35 step1.py
+@echo Using Python 3.4
+@python34 step1.py
--- python_projects/tkinter_tutorial/step1.py
+++ python_projects/tkinter_tutorial/step1.py
@@ -2,6 +2,15 @@ import sys
 if sys.version_info < (3,0):
     raise DeprecationWarning("Only supported for Python 3.x")
 
+import matplotlib
+matplotlib.use("TkAgg")
+# matplotlib.use("Agg")
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
+
+from matplotlib.figure import Figure
+
 import tkinter as tk
 from tkinter import ttk
 
@@ -11,8 +20,10 @@ LARGE_FONT = ("Verdana", 12)
 # Styling GUIs and windows in Python 3 - Tkinter tutorial Python 3.4
 #
 # https://www.youtube.com/watch?v=A0gaXfM1UN0 Video 2
+# https://www.youtube.com/watch?v=Y6cir7P3YUk Video 3
 # https://www.youtube.com/watch?v=jBUpjijYtCk Video 4
 # https://www.youtube.com/watch?v=oV68QJJUXTU Video 5
+# https://www.youtube.com/watch?v=Zw6M-BnAPP0 Video 6
 
 class SeaofBTCapp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -28,7 +39,7 @@ class SeaofBTCapp(tk.Tk):
 
         self.frames = {}
 
-        for f in (StartPage, PageOne, PageTwo):
+        for f in (StartPage, PageOne, PageTwo, PageThree):
             frame = f(container, self)
             self.frames[f] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -65,6 +76,14 @@ class StartPage(tk.Frame):
         )
         button2.pack()
 
+        button3 = ttk.Button(
+            self,
+            text="Visit Graph Page",
+            command=lambda: controller.show_frame(PageThree)
+        )
+        button3.pack()
+
+
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -85,6 +104,13 @@ class PageOne(tk.Frame):
         )
         button2.pack()
 
+        button3 = ttk.Button(
+            self,
+            text="Visit Graph Page",
+            command=lambda: controller.show_frame(PageThree)
+        )
+        button3.pack()
+
 class PageTwo(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -104,6 +130,36 @@ class PageTwo(tk.Frame):
             command=lambda: controller.show_frame(PageOne)
         )
         button2.pack()
+
+        button3 = ttk.Button(
+            self,
+            text="Visit Graph Page",
+            command=lambda: controller.show_frame(PageThree)
+        )
+        button3.pack()
+
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = ttk.Button(
+            self,
+            text="Back to Home",
+            command=lambda: controller.show_frame(StartPage)
+        )
+        button1.pack()
+
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
 
 app = SeaofBTCapp()
 app.mainloop()
