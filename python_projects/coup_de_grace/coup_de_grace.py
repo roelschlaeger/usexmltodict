e461d10cdae7253720fec000b18f67a1bf218c1e
roelschlaeger <roelschlaeger@gmail.com>
2016-02-27 11:58:51 -0600

    Adapt for Python27 and Python35

--- python_projects/coup_de_grace/coup_de_grace.py
+++ python_projects/coup_de_grace/coup_de_grace.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 TRANSLATE_PAIRS = {
     'B': '1',
     'Z': '0',
@@ -11,13 +13,37 @@ TRANSLATE_PAIRS = {
     'J': '9',
 }
 
-import string
-keys = "".join(TRANSLATE_PAIRS.keys())
-values = "".join(TRANSLATE_PAIRS.values())
-TABLE = string.maketrans(keys, values)
+KEYS = "".join(list(TRANSLATE_PAIRS.keys()))
+VALUES = "".join(list(TRANSLATE_PAIRS.values()))
 
-result = "XXDDD\nXDRRR".translate(TABLE)
-print(result)
+import sys
+if sys.version_info < (3,):
+    import string
+    TABLE = string.maketrans(KEYS, VALUES)
+else:
+    TABLE = str.maketrans(KEYS, VALUES)
 
-result = string.printable.translate(TABLE)
-print(result)
+
+def coup_de_grace(s):
+
+    s = s.upper()
+    s = s.replace("\n", "")
+    print(s)
+
+    result = s.translate(TABLE)
+    print(result)
+
+    latmin = result[0:2]
+    latminthou = result[2:5]
+    lonmin = result[5:7]
+    lonminthou = result[7:10]
+
+    latitude = "N38 %2s.%3s" % (latmin, latminthou)
+    longitude = "W90 %2s.%3s" % (lonmin, lonminthou)
+
+    return (latitude, longitude)
+
+
+latitude, longitude = coup_de_grace("XXDDD\nXDRRR")
+
+print(latitude, longitude)
