eeefebd1eb050df833cb6f201c753cec1f7e0e9d
roelschlaeger <roelschlaeger@gmail.com>
2016-05-12 14:25:31 -0500

    Adjust tag names to be globals

--- python_projects/rooter/make_rtept.py
+++ python_projects/rooter/make_rtept.py
@@ -1,9 +1,21 @@
-# -*- encoding="utf-8" -*-
+# -*- coding: utf-8 -*-
+
+"""
+make_rtept.py: Create a <rtept> Element for a <gpx> XML file.
+
+Create a ElementTree rtept element suitable for use with Microsoft Streets and
+Trips.
+"""
+
 ########################################################################
 
 from __future__ import print_function
+from xml.etree import ElementTree as ET
 
 ########################################################################
+
+__VERSION__ = "0.0.1"
+__CREATOR__ = "make_rtept.py %s" % __VERSION__
 
 GPX_NAMESPACE = "http://www.topografix.com/GPX/1/1"
 GSAK_NAMESPACE = "http://www.gsak.net/xmlv1/6"
@@ -11,20 +23,26 @@ GROUNDSPEAK_NAMESPACE = "http://www.groundspeak.com/cache/1/0/1"
 
 ITEM_TEXT = "MAKE_RTEPT_WAYPOINT"
 
-from xml.etree import ElementTree as ET
 ET.register_namespace("", GPX_NAMESPACE)
 ET.register_namespace("GSAK", GSAK_NAMESPACE)
 ET.register_namespace("groundspeak", GROUNDSPEAK_NAMESPACE)
+# ET.register_namespace("xsd", "http://www.w3.org/2001/XMLSchema")
+# ET.register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+########################################################################
+
 
 def get_usersort(w0):
+    """Get a UserSort value for the current waypoint w0, if one exists."""
     try:
+
         value = w0\
-.find(make_tag("extensions"))\
-.find(make_tag("wptExtension", GSAK_NAMESPACE))\
-.find(make_tag("UserSort", GSAK_NAMESPACE))\
-.text
+            .find(make_tag("extensions"))\
+            .find(make_tag("wptExtension", GSAK_NAMESPACE))\
+            .find(make_tag("UserSort", GSAK_NAMESPACE))\
+            .text
         # print("Success: %s" % value, list(w0))
-    except AttributeError as error:
+    except AttributeError:
         value = ""
 
     return value
@@ -33,12 +51,15 @@ def get_usersort(w0):
 
 
 class DummyText(object):
+    """Create an object from a text string that has a .text attribute."""
+
     text = ""
 
     def __init__(self, item):
+        """Create a DummyText object."""
         if item is None:
             self.text = ""
-        elif type(item) == type(""):
+        elif isinstance(item, str):
             self.text = item
         else:
             self.text = item.text
@@ -47,38 +68,42 @@ class DummyText(object):
 
 
 def make_tag(s, ns=GPX_NAMESPACE):
+    """Create an Element tag from tagname s and optional namespace ns."""
     return "{%s}%s" % (ns, s)
 
-ele_tag = make_tag("ele")
-time_tag = make_tag("time")
-magvar_tag = make_tag("magvar")
-geoidheight_tag = make_tag("geoidheight")
-name_tag = make_tag("name")
-cmt_tag = make_tag("cmt")
-desc_tag = make_tag("desc")
-src_tag = make_tag("src")
-link_tag = make_tag("link")
-sym_tag = make_tag("sym")
-rtype_tag = make_tag("type")
-fix_tag = make_tag("fix")
-sat_tag = make_tag("sat")
-hdop_tag = make_tag("hdop")
-vdop_tag = make_tag("vdop")
-pdop_tag = make_tag("pdop")
-ageofdgpsdata_tag = make_tag("ageofdgpsdata")
-dgpsid_tag = make_tag("dgpsid")
-extensions_tag = make_tag("extensions")
+########################################################################
 
-usersort_tag = make_tag("UserSort", GSAK_NAMESPACE)
-# print(usersort_tag)
+ELE_TAG = make_tag("ele")
+TIME_TAG = make_tag("time")
+MAGVAR_TAG = make_tag("magvar")
+GEOIDHEIGHT_TAG = make_tag("geoidheight")
+NAME_TAG = make_tag("name")
+CMT_TAG = make_tag("cmt")
+DESC_TAG = make_tag("desc")
+SRC_TAG = make_tag("src")
+LINK_TAG = make_tag("link")
+SYM_TAG = make_tag("sym")
+RTYPE_TAG = make_tag("type")
+FIX_TAG = make_tag("fix")
+SAT_TAG = make_tag("sat")
+HDOP_TAG = make_tag("hdop")
+VDOP_TAG = make_tag("vdop")
+PDOP_TAG = make_tag("pdop")
+AGEOFDGPSDATA_TAG = make_tag("ageofdgpsdata")
+DGPSID_TAG = make_tag("dgpsid")
+EXTENSIONS_TAG = make_tag("extensions")
 
-rtept_tag = make_tag("rtept")
+USERSORT_TAG = make_tag("UserSort", GSAK_NAMESPACE)
+
+RTEPT_TAG = make_tag("rtept")
+
+WPT_TAG = make_tag("wpt")
 
 ########################################################################
 
 
 def add_rtepts_from_wpts(rte, wpts):
-
+    """Create a series of <rtept> Elements appended to the <rte> Element."""
     # create a list for temporarily storing accumulating rtepts
     rtept_list = []
 
@@ -90,14 +115,14 @@ def add_rtepts_from_wpts(rte, wpts):
         lon = wpt.attrib["lon"]
 
         # make a new <wpt> element from the old one
-        new_wpt = ET.Element(wpt_tag, {"lat": lat, "lon": lon})
+        new_wpt = ET.Element(WPT_TAG, {"lat": lat, "lon": lon})
 
         # make an <rtept> element
-        rtept = ET.Element(rtept_tag, {"lat": lat, "lon": lon})
+        rtept = ET.Element(RTEPT_TAG, {"lat": lat, "lon": lon})
 
         # gather this information first
-        _name = DummyText(wpt.find(name_tag))
-        _desc = DummyText(wpt.find(desc_tag))
+        _name = DummyText(wpt.find(NAME_TAG))
+        _desc = DummyText(wpt.find(DESC_TAG))
         _usersort = DummyText(get_usersort(wpt))
 
         def _fabricated_name():
@@ -108,25 +133,25 @@ def add_rtepts_from_wpts(rte, wpts):
         # pass along most of the information to the new_wtp (but not
         # extensions)
         for tag in [
-            ele_tag,                           # KEEP THESE IN THIS ORDER #
-            time_tag,                          #
-            magvar_tag,                        #
-            geoidheight_tag,                   #
-            name_tag,                          #
-            cmt_tag,                           #
-            desc_tag,                          #
-            src_tag,                           #
-            link_tag,                          #
-            sym_tag,                           #
-            rtype_tag,                         #
-            fix_tag,                           #
-            sat_tag,                           #
-            hdop_tag,                          #
-            vdop_tag,                          #
-            pdop_tag,                          #
-            ageofdgpsdata_tag,                 #
-            dgpsid_tag,                        # KEEP THESE IN THIS ORDER #
-            # extensions_tag                   # not passing along extensions
+            ELE_TAG,                           # KEEP THESE IN THIS ORDER #
+            TIME_TAG,                          #
+            MAGVAR_TAG,                        #
+            GEOIDHEIGHT_TAG,                   #
+            NAME_TAG,                          #
+            CMT_TAG,                           #
+            DESC_TAG,                          #
+            SRC_TAG,                           #
+            LINK_TAG,                          #
+            SYM_TAG,                           #
+            RTYPE_TAG,                         #
+            FIX_TAG,                           #
+            SAT_TAG,                           #
+            HDOP_TAG,                          #
+            VDOP_TAG,                          #
+            PDOP_TAG,                          #
+            AGEOFDGPSDATA_TAG,                 #
+            DGPSID_TAG,                        # KEEP THESE IN THIS ORDER #
+            # EXTENSIONS_TAG                   # not passing along extensions
         ]:
 
             # check for the presence of the tag in the old wpt
@@ -140,11 +165,11 @@ def add_rtepts_from_wpts(rte, wpts):
                 item2.text = found.text
 
                 # interchange the <name> and <desc> tags
-                if tag == name_tag:
+                if tag == NAME_TAG:
                     item = ET.SubElement(rtept, tag)
                     # item.text = "%s %s" % (_usersort.text, _desc.text)
                     item.text = _fabricated_name()
-                elif tag == desc_tag:
+                elif tag == DESC_TAG:
                     item = ET.SubElement(rtept, tag)
                     item.text = _name.text
                 else:
@@ -155,8 +180,9 @@ def add_rtepts_from_wpts(rte, wpts):
         rte.append(new_wpt)
 
     # now transfer all of the collected <rtept> data
-    for rtept in rtept_list:
-        rte.append(rtept)
+#   for rtept in rtept_list:
+#       rte.append(rtept)
+    rte.extend(rtept_list)
 
 #######################################################################
 
@@ -164,15 +190,14 @@ if __name__ == "__main__":
 
     from gpx2kml import pretty_print
 
-    # not needed elsewhere
-    wpt_tag = make_tag("wpt")
-
     # get wpts elements from an existing file
     root = ET.parse("topo840b - collinsville IL.gpx").getroot()
-    wpts = root.findall(wpt_tag)
+    wpts = root.findall(WPT_TAG)
 
     # build an output gpx file
     gpx = ET.Element("gpx")
+    gpx.attrib["version"] = "1.1"
+    gpx.attrib["creator"] = __CREATOR__
 
     # add all current wpts as-is
     for wpt in wpts:
@@ -182,7 +207,7 @@ if __name__ == "__main__":
             # print("'%s'" % txt)
             if txt is not None and txt.strip() == "":
                 item.text = ""
-            if item.tag == rtype_tag:
+            if item.tag == RTYPE_TAG:
                 item.text = "%s %s" % (ITEM_TEXT, item.text)
         gpx.append(wpt)
 
@@ -198,3 +223,5 @@ if __name__ == "__main__":
     ofile.close()
 
     print("Output is in %s" % OUTNAME)
+
+# end of file
