6b3f6a618f609512e459a5bff6da47179f43dc0f
roelschlaeger <roelschlaeger@gmail.com>
2016-05-28 17:20:33 -0500

    First output with Polybius6

--- python_projects/khan_academy/20160517_clues/dibits.py
+++ python_projects/khan_academy/20160517_clues/dibits.py
@@ -4,6 +4,8 @@
 
 from __future__ import print_function
 
+DEBUG = False
+
 # The cipher in 9210801046_586c49b847_c.jpg is composed of two two-bit pairs,
 # as follows:
 #
@@ -77,61 +79,90 @@ DIBITS = [
     (3, 0), (0, 2)
 ]
 
-"""The DIBITS values each form four-bit values. Three of these four-bit values
-are split into four three-bit values to be used as XOR masks for Polybius row
-and column"""
+########################################################################
 
 
-def three(a, b, c):
-    """Combine three dibit pairs into two tribit pairs."""
+def binary6(item):
+    """Convert numeric item into 6 character binary string."""
+    out = []
+    for _ in range(6):
+        item, r = divmod(item, 2)
+        out.append(chr(ord('0') + r))
+    out.reverse()
+    return "".join(out)
+
+########################################################################
+
+
+def print_binary_items(*items):
+    """Convert all items to 6 character binary strings."""
+    # print(items)
+    line = []
+    for item in items:
+        line.append("".join(binary6(item)))
+    print(line)
+
+########################################################################
+
+
+def hex(a, b, c):
+    """Convert three 4-bit values to two 6-bit values."""
     a1, a2 = a
     assert 0 <= a1 <= 3, "a1 error: %s" % a1
-    assert 0 <= a2 <= 3, "a1 error: %s" % a2
+    assert 0 <= a2 <= 3, "a2 error: %s" % a2
 
     b1, b2 = b
     assert 0 <= b1 <= 3, "b1 error: %s" % b1
-    assert 0 <= b2 <= 3, "b1 error: %s" % b2
+    assert 0 <= b2 <= 3, "b2 error: %s" % b2
 
     c1, c2 = c
     assert 0 <= c1 <= 3, "c1 error: %s" % c1
-    assert 0 <= c2 <= 3, "c1 error: %s" % c2
+    assert 0 <= c2 <= 3, "c2 error: %s" % c2
 
-    n1 = a1 << 4 | a2 << 2 | b1
-    n2 = b2 << 4 | c1 << 2 | c2
-    n11, n12 = divmod(n1, 8)
-    n21, n22 = divmod(n2, 8)
-    return [(n11, n12), (n21, n22)]
+    o1 = a1 << 4 | a2 << 2 | b1
+    o2 = b2 << 4 | c1 << 2 | c2
+
+    if DEBUG:
+        print_binary_items(a1, a2, b1, b2, c1, c2, o1, o2)
+
+    return o1, o2
+
+########################################################################
 
 
-def three2four(l):
-    """Convert three dibit pairs to four tribit pairs."""
+def three2two(l):
+    """Convert three dibit pairs to two hexbit pairs."""
     #
-    def generator(l):
+    def generator():
         """Create a generator for the items in DIBITS."""
         for x in l:
             yield x
 
-    g = generator(l)
+    g = generator()
     out = []
 
     for i in range(0, len(l), 3):
-        b1 = next(g, (0, 0))
-        b2 = next(g, (0, 0))
-        b3 = next(g, (0, 0))
+        a = next(g, (0, 0))
+        b = next(g, (0, 0))
+        c = next(g, (0, 0))
         # print(b1, b2, b3)
-        out.extend(three(b1, b2, b3))
+        out.extend(hex(a, b, c))
 
     return out
 
-TRIBITS = three2four(DIBITS)
+HEXBITS = three2two(DIBITS)
+
+########################################################################
 
 if __name__ == "__main__":
 
     from pprint import pprint
     import sys
     if sys.version_info > (3, 0):
-        pprint(TRIBITS, width=76, compact=True)
+        print("HEXBITS =")
+        pprint(HEXBITS, width=76, compact=True)
     else:
-        pprint(TRIBITS, width=76)
+        print("HEXBITS =")
+        pprint(HEXBITS, width=76)
 
 # end of file
--- python_projects/khan_academy/20160517_clues/tribits.py
+++ python_projects/khan_academy/20160517_clues/tribits.py
@@ -2,23 +2,39 @@
 """Pairs of three-bit values used to XOR Polybius (row, col) values."""
 
 from __future__ import print_function
-from polybius import Polybius5
+from polybius6 import Polybius6
 
-TRIBITS = [
-    (4, 3), (3, 2), (5, 4), (1, 5), (6, 0), (6, 2), (4, 2), (4, 2), (7, 2),
-    (0, 7), (6, 4), (3, 4), (0, 7), (4, 1), (0, 2), (4, 3), (6, 6), (3, 1),
-    (2, 0), (3, 2), (2, 5), (3, 0), (1, 5), (6, 1), (5, 2), (1, 0), (4, 2),
-    (5, 5), (4, 1), (3, 1), (2, 7), (3, 2), (6, 3), (2, 3), (4, 2), (6, 4),
-    (3, 1), (6, 0), (7, 2), (2, 5), (5, 0), (4, 0), (2, 3), (6, 2), (6, 6),
-    (1, 7), (0, 5), (4, 3), (3, 7), (3, 6), (4, 4), (4, 3), (0, 3), (4, 4),
-    (2, 1), (6, 3), (2, 2), (3, 7), (2, 0), (4, 0), (6, 2), (3, 7), (2, 0),
-    (2, 1), (1, 4), (5, 5), (1, 2), (3, 3), (6, 1), (4, 1), (3, 2), (4, 0),
-    (0, 5), (3, 7), (0, 1), (0, 7), (5, 4), (3, 1), (1, 2), (0, 5), (1, 3),
-    (5, 4), (7, 6), (3, 5), (4, 0), (4, 6), (6, 7), (0, 0), (6, 1), (5, 4),
-    (2, 4), (2, 3), (0, 4), (3, 1), (5, 0), (5, 4), (4, 0), (3, 2), (4, 7),
-    (5, 1), (1, 0), (5, 7), (4, 2), (2, 5), (6, 0), (4, 0)
+#   TRIBITS = [
+#       (4, 3), (3, 2), (5, 4), (1, 5), (6, 0), (6, 2), (4, 2), (4, 2), (7, 2),
+#       (0, 7), (6, 4), (3, 4), (0, 7), (4, 1), (0, 2), (4, 3), (6, 6), (3, 1),
+#       (2, 0), (3, 2), (2, 5), (3, 0), (1, 5), (6, 1), (5, 2), (1, 0), (4, 2),
+#       (5, 5), (4, 1), (3, 1), (2, 7), (3, 2), (6, 3), (2, 3), (4, 2), (6, 4),
+#       (3, 1), (6, 0), (7, 2), (2, 5), (5, 0), (4, 0), (2, 3), (6, 2), (6, 6),
+#       (1, 7), (0, 5), (4, 3), (3, 7), (3, 6), (4, 4), (4, 3), (0, 3), (4, 4),
+#       (2, 1), (6, 3), (2, 2), (3, 7), (2, 0), (4, 0), (6, 2), (3, 7), (2, 0),
+#       (2, 1), (1, 4), (5, 5), (1, 2), (3, 3), (6, 1), (4, 1), (3, 2), (4, 0),
+#       (0, 5), (3, 7), (0, 1), (0, 7), (5, 4), (3, 1), (1, 2), (0, 5), (1, 3),
+#       (5, 4), (7, 6), (3, 5), (4, 0), (4, 6), (6, 7), (0, 0), (6, 1), (5, 4),
+#       (2, 4), (2, 3), (0, 4), (3, 1), (5, 0), (5, 4), (4, 0), (3, 2), (4, 7),
+#       (5, 1), (1, 0), (5, 7), (4, 2), (2, 5), (6, 0), (4, 0)
+#   ]
+
+# Each entry in HEXBITS represents six bits extracted from the
+# 9210801046_586c49b847_c.jpg image as described in dibits.py
+# The entries in TRIBITS are the pairs of three-bit numbers used to generate
+# each of the HEXBITS entries. For example, (4, 3) ==> 4 * 8 + 3 = 35.
+# Each number represents one character.
+
+HEXBITS = [
+    35, 26, 44, 13, 48, 50, 34, 34, 58, 7, 52, 28, 7, 33, 2, 35, 54, 25, 16,
+    26, 21, 24, 13, 49, 42, 8, 34, 45, 33, 25, 23, 26, 51, 19, 34, 52, 25, 48,
+    58, 21, 40, 32, 19, 50, 54, 15, 5, 35, 31, 30, 36, 35, 3, 36, 17, 51, 18,
+    31, 16, 32, 50, 31, 16, 17, 12, 45, 10, 27, 49, 33, 26, 32, 5, 31, 1, 7,
+    44, 25, 10, 5, 11, 44, 62, 29, 32, 38, 55, 0, 49, 44, 20, 19, 4, 25, 40,
+    44, 32, 26, 39, 41, 8, 47, 34, 21, 48, 32
 ]
 
+# PAD is a transcription of the newspaper article found at the scene
 PAD = """
 The whole grain goodness of
 blue chip dividend stocks has
@@ -33,66 +49,116 @@ ket signalling an expectation of
 rising interest rates, the five-year
 rally for steady blue-chip divi-
 dend payers has been stalled.
+Should you be scared if you own
+a lot of these stocks either directly
+or through mutual funds or
+exchange-traded funds? David
+Baskin, president of Baskin Finan-
+cial Services, has a two-pronged
+answer: Keep your top-quality
+dividend stocks, but be prepared
+to follow his firm's example in
+trimming holdings in stucks such
+as TransCanada Corp., Keyera
+Corp. and Pembina Pipeline
+Corp.
+Let's have Mr. Baskin run us
+through his thinking on dividend
+stocks, which are a bit part of the
+portfolios his firm puts together
+for clients. A mini-manifesto
 """
 
 PAD_CHARS = "".join([x for x in PAD.upper() if x.isalpha()])
 
+# adjust length of PAD_CHARS to a multiple of 6
+if (len(PAD_CHARS) % 6):
+    PAD_CHARS += "Z" * (6 - (len(PAD_CHARS) % 6))
+
+# print(len(PAD_CHARS))
+
+
+def pad_mask(s, debug=False):
+    s = list(s)
+
+    def isvowel(c):
+        if c in "AEIOUY":
+            return 1
+        return 0
+
+    while s:
+        out = 0
+        for _ in range(6):
+            c = s.pop(0)
+            n = isvowel(c)
+            out = out * 2 + n
+
+            if debug:
+                print(c, n, sep="", end=" ")
+
+        if debug:
+            print(end="\n")
+
+        yield out
+
+#   generator = pad_mask(PAD_CHARS, False)
+#   while 1:
+#       print(generator.next())
+
 ########################################################################
 
 
-def decrypt_one_time_pad(pb, tribits, pad_chars, debug=False):
+def decrypt_one_time_pad(pb, hexbits, pad_chars, debug=False):
     """Decrypt the tribits message using the one-time pad."""
+
+    generator = pad_mask(PAD_CHARS)
+
     if debug:
-        print("tribits   length: ", len(tribits))
+        print("hexbits   length: ", len(hexbits))
         print("pad_chars length: ", len(pad_chars))
         print()
 
+    print("idx   t  x  v n1 n2 c")
+    print("---   -  -  -  -  - -")
     out = []
-    for index, t in enumerate(tribits):
-        t1, t2 = t
+    for index, t in enumerate(hexbits):
+        x = generator.next()
+        v = t ^ x
+        n1, n2 = divmod(v, 8)
+        try:
+            c = pb.polybius_char(n1, n2)
+        except AssertionError:
+            c = "?"
         if debug:
-            print(t1, t2)
-        n1, n2 = pb.polybius_pair(pad_chars[index])
-        if debug:
-            print(n1, n2)
-            print("----")
-        n1 = n1 ^ t1
-        n2 = n2 ^ t2
-        if debug:
-            print(n1, n2)
-            print()
-        out.append(pb.polybius_char(n1, n2))
+            print("%3d: %2d %2d %2d %2d %2d %c" % (index, t, x, v, n1, n2, c))
+        out.append(c)
     return "".join(out)
 
 ########################################################################
 
 if __name__ == '__main__':
 
-    # AFKPU
-    # BGLQV
-    # CHMRW
-    # DINSX
-    # EJOTY
-    # key1 = "AFKPUBGLQVCHMRWDINSXEJOTY"
+    from pprint import pprint
 
-    # EJOTY
-    # DINSX
-    # CHMRW
-    # BGLQV
-    # AFKPU
-    PB = Polybius5()
-    key2 = "EJOTYDINSXCHMRWBGLQVAFKPU"
+    def printby(s, n):
+        """Print the 's' string in 'n' character chunks."""
+        out = []
+        while s:
+            t, s = s[:n], s[n:]
+            out.append(t)
+        pprint(out)
+
+    # printby(PAD_CHARS, 6)
+    generator = pad_mask(PAD_CHARS)
+    # from dibits import binary6
+    # pprint([(x, binary6(x)) for x in generator])
+    # print()
+
+    PB = Polybius6()
+    key2 = "FGHIJK EXYZ0L DW781M CV692N BU543O ATSRQP"
     PB.set_key(key2)
     PB.print_polybius()
-
-#   print(PAD)
-#   print(PAD_CHARS)
-
-#   PAD_PAIRS = PB.text2pairs(PAD_CHARS)
-#   from pprint import pprint
-#   pprint(PAD_PAIRS, width=78)
-
-    cleartext = decrypt_one_time_pad(PB, TRIBITS, PAD_CHARS, True)
+    cleartext = decrypt_one_time_pad(PB, HEXBITS, PAD_CHARS, True)
     print(cleartext)
 
 # end of file
