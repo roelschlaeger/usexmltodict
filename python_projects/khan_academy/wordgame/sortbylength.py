652b6a87ca24dc4b6282abc8fb791ffb1a42e148
roelschlaeger <roelschlaeger@gmail.com>
2016-06-20 13:48:59 -0500

    Initial checkin

---
+++ python_projects/khan_academy/wordgame/sortbylength.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# vim:ts=4:sw=4:tw=0:wm=0:et:foldlevel=99:fileencoding=utf-8:ft=python
+
+# Created:       Mon 20 Jun 2016 12:53:42 PM CDT
+# Last Modified: Mon 20 Jun 2016 01:42:46 PM CDT
+
+"""
+SYNOPSIS
+
+    TODO helloworld [-h | --help] [-v | --version] [--verbose]
+
+DESCRIPTION
+
+    TODO This describes how to use this script.
+    This docstring will be printed by the script if there is an error or
+    if the user requests help (-h or --help).
+
+EXAMPLES
+
+    TODO: Show some examples of how to use this script.
+
+EXIT STATUS
+
+    TODO: List exit codes
+
+AUTHOR
+
+    TODO: Robert Oelschlaeger <roelsch2009@gmail.com>
+
+LICENSE
+
+    This script is in the public domain.
+
+VERSION
+
+"""
+
+from __future__ import print_function
+
+__VERSION__ = "0.0.1"
+
+########################################################################
+
+WORDS = [
+    "accommodate",
+    "arctic",
+    "caribbean",
+    "cemetery",
+    "conscience",
+    "convalesce",
+    "deductible",
+    "ecstasy",
+    "embarrass",
+    "handkerchief",
+    "harass",
+    "indict",
+    "liaison",
+    "maintenance",
+    "millennium",
+    "occurred",
+    "pharoah",
+    "playwright",
+    "pronunciation",
+    "recommend",
+    "rhythm",
+    "supercede",
+    "wierd",
+]
+
+
+print(",\n".join("'%s'" % x for x in sorted(WORDS, key=len)))
+
+if __name__ == '__main__':
+
+    import argparse
+    import os
+    import sys
+    import textwrap
+    import time
+    import traceback
+
+#from pexpect import run, spawn
+
+# Uncomment the following section if you want readline history support.
+#import readline, atexit
+#histfile = os.path.join(os.environ['HOME'], '.TODO_history')
+#try:
+#    readline.read_history_file(histfile)
+#except IOError:
+#    pass
+#atexit.register(readline.write_history_file, histfile)
+
+########################################################################
+
+    def main():
+
+        global OPTIONS
+
+        # TODO: Do something more interesting here...
+        print('Hello world!')
+
+########################################################################
+
+    try:
+        START_TIME = time.time()
+
+        PARSER = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            usage=textwrap.dedent(globals()['__doc__']),
+            version="Version: %s" % __VERSION__
+        )
+
+        PARSER.add_argument(
+            '--verbose',
+            action='store_true',
+            default=False,
+            help='verbose output'
+        )
+
+        OPTIONS = PARSER.parse_args()
+
+        if OPTIONS.verbose:
+            print(time.asctime())
+
+        EXIT_CODE = main()
+
+        if EXIT_CODE is None:
+            EXIT_CODE = 0
+
+        if OPTIONS.verbose:
+            print(time.asctime())
+            print('TOTAL TIME IN MINUTES:',)
+            print((time.time() - START_TIME) / 60.0)
+
+        sys.exit(EXIT_CODE)
+
+    except KeyboardInterrupt as error_exception:       # Ctrl-C
+        raise error_exception
+
+    except SystemExit as error_exception:              # sys.exit()
+        raise error_exception
+
+    except Exception as error_exception:
+        print('ERROR, UNEXPECTED EXCEPTION')
+        print(str(error_exception))
+        traceback.print_exc()
+        os._exit(1)
+
+# end of file
