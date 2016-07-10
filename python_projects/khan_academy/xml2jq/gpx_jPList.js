655a096cbb5a850e6d6135131a6d3f0ef9a42dd5
roelschlaeger <roelschlaeger@gmail.com>
2016-07-10 15:30:26 -0500

    Pagination?

---
+++ python_projects/khan_academy/xml2jq/gpx_jPList.js
@@ -0,0 +1,61 @@
+<!-- http://jplist.com/documentation/js-settings -->
+
+var fillListDiv = function() {
+    var $list = $(".list");
+    $list.empty();
+
+
+    var wpts = data.gpx.wpt;
+    wpts.forEach(function(wpt, index) {
+        $item = $("<div/>")
+            .addClass("list-item")
+            .appendTo($list);
+
+        $table = $("<table/>")
+            .append("<caption/>")
+            .append("<thead/>")
+            .append("<tbody/>")
+            .appendTo($item);
+
+        $thead = $table.find("thead");
+        // console.log($thead);
+        $row = $("<tr/>").appendTo($thead);
+        keys = ["lat", "lon", "desc", "name", "sym", "time", "type"];
+        keys.forEach(
+            function(element) {
+                $row.append($("<th/>").text(element));
+            });
+
+        // console.log(wpt);
+        var $row = $("<tr/>").appendTo($table);
+
+        var lat = wpt["@lat"];
+        var lon = wpt["@lon"];
+        var desc = wpt.desc;
+        // var extensions = wpt.extensions;
+        // var link = wpt.link;
+        var name = wpt.name;
+        var sym = wpt.sym;
+        // console.log(sym);
+        var time = wpt.time;
+        var type = wpt.type;
+        var text = {
+            "lat": lat,
+            "lon": lon,
+            "desc": desc,
+            // "extensions": extensions,
+            // "link": link,
+            "name": name,
+            "sym": sym,
+            "time": time,
+            "type": type,
+        };
+
+        $.each(text, function(name, property){
+            var td = $("<td/>").addClass(name).text(property);
+            $row.append(td);
+        });
+
+        $item.append($("<br/>"));
+    });
+};
