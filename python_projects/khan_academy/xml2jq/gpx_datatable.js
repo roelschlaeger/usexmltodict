7066fdda3eaa955a4850ac9625acef638fb43993
roelschlaeger <roelschlaeger@gmail.com>
2016-07-30 12:51:57 -0500

    Add popover for short and long descriptions

--- python_projects/khan_academy/xml2jq/gpx_datatable.js
+++ python_projects/khan_academy/xml2jq/gpx_datatable.js
@@ -2,10 +2,10 @@
 
 "use strict";
 
-var fillListDiv = function() {
-    var $list = $(".row .panel .table tbody");
+var fillTable = function() {
+    var $list = $("#example tbody");
 
-    // $list.empty();
+    $list.empty();
 
     var wpts = data.gpx.wpt;
     wpts.forEach(function(wpt, index) {
@@ -13,8 +13,28 @@ var fillListDiv = function() {
         var lat = latstr(wpt["@lat"]);
         var lon = lonstr(wpt["@lon"]);
         var desc = wpt.desc;
-        // var extensions = wpt.extensions;
-        console.log(wpt.extensions);
+
+        var short = wpt.extensions["groundspeak:cache"]["groundspeak:short_description"]
+        var short_text = short["#text"];
+        if (short["@html"] === "True") {
+            short_text = $("<div/>").html(short_text).text();
+        }
+
+        var long = wpt.extensions["groundspeak:cache"]["groundspeak:long_description"]
+        var long_text = long["#text"];
+        if (long["@html"] === "True") {
+            long_text = $("<div/>").html(long_text).text();
+        }
+
+        // if(index === 1) {
+        //     console.log(Object.keys(wpt.extensions["groundspeak:cache"]));
+        //     console.log(Object.keys(wpt.extensions["gsak:wptExtension"]));
+        //     console.log(short["#text"]);
+        //     console.log(long["#text"]);
+        //     console.log(short_text);
+        //     console.log(long_text);
+        // }
+
         var hint = wpt.extensions["groundspeak:cache"]["groundspeak:encoded_hints"]
         var container = wpt.extensions["groundspeak:cache"]["groundspeak:container"]
         var usersort = wpt.extensions["gsak:wptExtension"]["gsak:UserSort"]
@@ -24,20 +44,39 @@ var fillListDiv = function() {
         //     var time = wpt.time;
         var type = wpt.type.replace("Geocache|", "").replace(" Cache", "");
 
+        // create the <tr> for the row
         var $tr = $("<tr/>")
             .appendTo($list);
 
+        // append <td>s to it for UserSort
         var $td = $("<td/>")
             .appendTo($tr)
             .text(usersort);
 
+        // append short and long descriptions to the name as a popover
+        if (!(short_text || long_text)) {
+            var $a = $("<span/>").text(name)
+        } else {
+            var $a = $("<a/>")
+                .attr("href", "#")
+                .attr("data-toggle", "popover")
+                .attr("data-trigger", "focus")
+                .attr("data-title", short_text)
+                .attr("data-content", long_text)
+                // .attr("text-decoration", "underline")
+                .text(name);
+        }
+
         $td = $("<td/>")
-            .appendTo($tr)
-            .text(name);
+            .append($a)
+            .appendTo($tr);
 
         if (link["@href"]) {
+            // var tlink = link["@href"];
+            var tlink = link["@href"].replace("cache_details", "cdpf") + "&lc=10";
             var descplus = $("<a/>")
-                .attr("href", link["@href"])
+                .attr("href", tlink)
+                .attr("target", "_blank")
                 .text(desc);
         } else {
             var descplus = desc;
@@ -66,6 +105,7 @@ var fillListDiv = function() {
         $td = $("<td/>")
             .appendTo($tr)
             .html(lon);
-
     });
+
+    $('[data-toggle="popover"]').popover();
 };
