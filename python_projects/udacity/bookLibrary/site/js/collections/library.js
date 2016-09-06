9685276320b4b00e36db2f932252fd4cc9f934c7
roelschlaeger <roelschlaeger@gmail.com>
2016-09-06 11:19:28 -0500

    Initial checkpoint

---
+++ python_projects/udacity/bookLibrary/site/js/collections/library.js
@@ -0,0 +1,9 @@
+// site/js/collections/library.js
+
+"use strict";
+
+var app = app || {};
+
+app.Library = Backbone.Collection.extend({
+    model: app.Book
+});
---
+++ python_projects/udacity/bookLibrary/site/js/models/book.js
@@ -0,0 +1,15 @@
+// site/js/models/book.js
+
+"use strict";
+
+var app = app || {};
+
+app.Book = Backbone.Model.extend({
+    defaults: {
+        coverImage: 'img/placeholder.png',
+        title: 'No title',
+        author: 'Unknown',
+        releaseDate: 'Unknown',
+        keywords: 'None'
+    }
+});
