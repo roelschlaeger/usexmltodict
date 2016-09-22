5883fdbe893a5f23cf9d6be25213fafdec6ee3d0
roelschlaeger <roelschlaeger@gmail.com>
2016-09-22 14:19:02 -0500

    open_labyrinth: tree built

--- python_projects/chkio/open_labyrinth/checkio.py
+++ python_projects/chkio/open_labyrinth/checkio.py
@@ -7,6 +7,7 @@
 # http://stackoverflow.com/questions/3097556/programming-theory-solve-a-maze
 
 from __future__ import print_function
+import copy
 from pprint import pprint
 
 DIRECTIONS = [
@@ -14,7 +15,7 @@ DIRECTIONS = [
     ("S", 1, 0),
     ("W", 0, -1),
     ("N", -1, 0),
-    ]
+]
 
 
 def checkio(s):
@@ -22,20 +23,39 @@ def checkio(s):
     rows = len(s)
     cols = len(s[0])
 
+    next = []
     tree = {}
-    import copy
+
     s2 = copy.copy(s)
-    for r in range(1, rows - 1):
-        for c in range(1, cols - 1):
-            l = []
-            s2[r][c] = 2
-            for dir, ro, co in DIRECTIONS:
-                if s2[r+ro][c+co] == 0:
-                    l.append((r+ro, c+co))
-            if l:
-                tree[(r, c)] = l
+    next.append((1, 1))
+
+    while next:
+        r, c = next.pop(0)
+        l = []
+        for dir, roffset, coffset in DIRECTIONS:
+            r2, c2 = r + roffset, c + coffset
+            # already visited?
+            if not (r2, c2) in tree:
+                # available?
+                if s2[r2][c2] == 0:
+                    l.append((r2, c2))
+                    next.append((r2, c2))
+        tree[(r, c)] = l
+        print((r, c), l)
+
+    def sprint(s):
+        result = []
+        for row in range(rows):
+            out = ""
+            for col in range(cols):
+                out += "%d" % s[row][col]
+            print(out)
+            result.append(out)
+        print()
+        return result
 
     def treeprint(d):
+        result = []
         for r in range(rows):
             out = ""
             for c in range(cols):
@@ -44,10 +64,21 @@ def checkio(s):
                 else:
                     out += "1"
             print(out)
+            result.append(out)
         print()
+        return result
 
-    treeprint(tree)
-    pprint(tree)
+    r1 = " ".join(sprint(s))
+    r2 = " ".join(treeprint(tree))
+    print(r1)
+    print(r2)
+    out = ""
+    for i in range(len(r1)):
+        out += (" " if r1[i] == r2[i] else "^")
+    print(out)
+    pprint(s)
+
+#   pprint(tree)
 
     if (0):
         stack = [(1, 1, [], s)]
@@ -56,8 +87,8 @@ def checkio(s):
             r, c, l, s = stack.pop()
             s[r][c] = 2  # mark location as having been visited
             for direction, ro, co in DIRECTIONS:
-                if s[r+ro][c+co] == 0:
-                    l.append((r+ro, c+co, s))
+                if s[r + ro][c + co] == 0:
+                    l.append((r + ro, c + co, s))
             break
 
         pprint(stack)
