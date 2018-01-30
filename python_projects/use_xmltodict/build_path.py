1681e057ace3afba41408a7ff669a5f1770667ec
roelschlaeger <roelschlaeger@gmail.com>
2018-01-30 12:29:23 -0600

    Add single_route_flag

--- python_projects/use_xmltodict/build_path.py
+++ python_projects/use_xmltodict/build_path.py
@@ -1,7 +1,7 @@
 # vim:ts=4:sw=4:tw=0:wm=0:et:noic
 
 
-def build_paths(data):
+def build_paths(data, single_route_flag=False):
     """Build list of (start, end, waypoints) tuples for a route."""
     # lats, lons, texts, syms, names = list(zip(*data)][4]
     names = list(zip(*data))[4]
@@ -11,21 +11,26 @@ def build_paths(data):
     # tuples of (start, end, waypoints[])
     results = []
 
-    for index, name in enumerate(names):
-        if (index == 0):
-            waypoints = []
-            start = index
-        elif name.startswith("GC"):
-            end = index
-            results.append((start, end, waypoints))
-            start = index
-            waypoints = []
-        elif not name.startswith("GC"):
-            waypoints.append(index)
+    if single_route_flag:
+        start = 0
+        end = len(names) - 1
+        results.append((start, end, list(range(1, end))))
+    else:
+        for index, name in enumerate(names):
+            if (index == 0):
+                waypoints = []
+                start = index
+            elif name.startswith("GC"):
+                end = index
+                results.append((start, end, waypoints))
+                start = index
+                waypoints = []
+            elif not name.startswith("GC"):
+                waypoints.append(index)
 
-    # go from last location back to first
-    end = 0
-    results.append((start, end, waypoints))
+        # go from last location back to first
+        end = 0
+        results.append((start, end, waypoints))
 
     return results
 
@@ -36,7 +41,7 @@ if __name__ == "__main__":
 
     data = get_data()
 
-    results = build_paths(data)
+    results = build_paths(data, True)
 
     for start, end, waypoints in results:
         print()
